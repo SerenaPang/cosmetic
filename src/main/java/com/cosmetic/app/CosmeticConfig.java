1964a5c91007e95b46247f94ce223b69ba513b0b
SerenaPang4 <ypang4@usfca.edu>
2025-02-28 11:47:45 -0800

    updated pom. xml

---
+++ src/main/java/com/cosmetic/app/CosmeticConfig.java
@@ -0,0 +1,10 @@
+package com.cosmetic.app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = { "com.cosmetic.app" })
+public class CosmeticConfig {
+
+}
---
+++ src/test/java/com/cosmetic/app/CosmeticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmetic.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmeticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
