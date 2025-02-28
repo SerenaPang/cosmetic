1d46f5088bccc6c3e8a3c79e2430feb3078482f8
SerenaPang4 <ypang4@usfca.edu>
2025-02-28 15:33:57 -0800

    added cosmetic web service

---
+++ src/main/java/com/cosmetic/app/webservices/SearchCosmeticsWebService.java
@@ -0,0 +1,29 @@
+package com.cosmetic.app.webservices;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cosmetic.app.model.Cosmetic;
+import com.cosmetic.app.service.CosmeticService;
+
+@RestController
+public class SearchCosmeticsWebService {
+	@Autowired
+	private CosmeticService cosmeticService;
+
+	// curl -X GET "http://localhost:8080/searchByQuery?query=mycosmetic"
+	@GetMapping("/searchByQuery")
+	public ResponseEntity<List<Cosmetic>> searchByQuery(
+			@RequestParam(value = "query", defaultValue = "nothing") String query) {
+		System.out.println("SearchProductsWebservice.searchByQuery() " + query);
+		List<Cosmetic> cosmetics = cosmeticService.searchBySearchString(query);
+
+		return ResponseEntity.status(HttpStatus.OK).body(cosmetics);
+	}
+}
