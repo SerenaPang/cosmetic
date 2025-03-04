3e5a9749024263d725486d0c7ee1642f597f7f94
SerenaPang4 <ypang4@usfca.edu>
2025-03-03 17:39:32 -0800

    updated script.js

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -29,7 +29,56 @@ function searchCosmeticProductAjaxCall(cosmeticName){
 		});
 }
 
+function renderCosmetics(cosmetics){
+	// creates a <table> element and a <tbody> element
+	   const tbl = document.createElement("table");
+	   const resultsDiv = document.getElementById("results");
 
+	   // Remove Previous results
+	   while (resultsDiv.firstChild) {
+	       resultsDiv.removeChild(resultsDiv.firstChild);
+	   }
+
+	   const firstRow = document.createElement("tr");
+	   insertTableHeader(firstRow, "Name");
+	   insertTableHeader(firstRow, "Description");
+	   insertTableHeader(firstRow, "Brand");
+	   insertTableHeader(firstRow, "Price");
+	   insertTableHeader(firstRow, "Order");
+	   tbl.appendChild(firstRow);
+
+	   // creating all cells
+	   for (let i = 0; i < products.length; i++) {
+	       let product = products[i];
+	       // creates a table row
+	       const row = document.createElement("tr");
+
+	       insertTableField(row, cosmetic.name);
+	       insertTableField(row, cosmetic.description);
+	       insertTableField(row, cosmetic.brand);
+	       insertTableField(row, cosmetic.price);
+	       insertAddToCartButton(row, cosmetic.id);
+
+	       // add the row to the end of the table body
+	       tbl.appendChild(row);
+	   }
+	   // appends <table> into <body>
+	   resultsDiv.appendChild(tbl);
+}
+
+function insertTableHeader(row, text) {
+    let cell = document.createElement("th");
+    let cellText = document.createTextNode(text);
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+}
+
+function insertTableField(row, text) {
+    let cell = document.createElement("td");
+    let cellText = document.createTextNode(text);
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+}
 
 
 
