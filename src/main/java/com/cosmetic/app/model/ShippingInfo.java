d08fd0742387385ff697feeb026921b68d7b09ca
SerenaPang4 <ypang4@usfca.edu>
2025-04-03 15:57:34 -0700

    added shpping info class

--- src/main/java/com/cosmetic/app/model/ShippingInfo.java
+++ src/main/java/com/cosmetic/app/model/ShippingInfo.java
@@ -4,6 +4,9 @@ public class ShippingInfo {
 	private String name;
 	private String address;
 	private String zipcode;
+	private String country;
+	private String city;
+	private String phone;
 
 	public String getName() {
 		return name;
@@ -29,8 +32,33 @@ public class ShippingInfo {
 		this.zipcode = zipcode;
 	}
 
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
 	@Override
 	public String toString() {
-		return "ShppingInfo [name=" + name + ", address=" + address + ", zipcode=" + zipcode + "]";
+		return "ShippingInfo [name=" + name + ", address=" + address + ", zipcode=" + zipcode + ", country=" + country
+				+ ", city=" + city + ", phone=" + phone + "]";
 	}
 }
