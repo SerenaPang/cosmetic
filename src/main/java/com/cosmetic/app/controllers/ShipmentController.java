0b25437c752cf2e704910365196e005fad844211
SerenaPang4 <ypang4@usfca.edu>
2025-04-03 20:28:50 -0700

    added shipping information functionality

--- src/main/java/com/cosmetic/app/controllers/ShipmentController.java
+++ src/main/java/com/cosmetic/app/controllers/ShipmentController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.cosmetic.app.model.ShippingInfo;
 import com.cosmetic.app.service.ShippingService;
 
 @Controller
@@ -17,5 +18,13 @@ public class ShipmentController {
 		
 		//System.out.println("name: " + name + );
 		return "shipping.html";
+	}
+	
+	@PostMapping("/shippingAddress")
+	public String ShippingAddress(ShippingInfo shippingInfo) {
+
+		System.out.println("Shipping info: " + 	shippingInfo);
+
+		return "payment.html";
 	}
 }
