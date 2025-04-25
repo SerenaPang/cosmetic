21f9170dc1004c0848ffebaea15e3d7c01fd4ee1
SerenaPang4 <ypang4@usfca.edu>
2025-04-24 17:33:59 -0700

    updated find by id implementation

--- src/main/java/com/cosmetic/app/postgressql/CosmeticRepositoryImpl.java
+++ src/main/java/com/cosmetic/app/postgressql/CosmeticRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.cosmetic.app.postgressql;
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,6 +21,7 @@ import com.cosmetic.app.repository.PostgreSQLConnection;
 public class CosmeticRepositoryImpl implements CosmeticRepository {
 	@Autowired
 	private PostgreSQLConnection dataSource;
+
 	@Override
 	public List<Cosmetic> findBySearchString(String searchString) {
 		System.out.println("CosmeticRepositoryImpl.findBySearchString() " + searchString);
@@ -38,41 +40,50 @@ public class CosmeticRepositoryImpl implements CosmeticRepository {
 
 	@Override
 	public Cosmetic findById(long id) {
-		System.out.println("CosmeticRepositoryImpl.findById() " + id);
-		Map<Long, Cosmetic> cosmetics = new HashMap<Long, Cosmetic>();
-		for (int i = 1; i < 10; i++) {
+		System.out.println("jdbc save Cosmetic");
+		try (Connection connection = dataSource.getConnection()) {
+			String sql = "SELECT id, name, description, brand, price FROM Cosmetic WHERE id =?";
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setLong(1, id);
 			Cosmetic cosmetic = new Cosmetic();
-			cosmetic.setId(i);
-			cosmetic.setName("Blush" + i);
-			cosmetic.setBrand("FlowerKnows" + i);
-			cosmetic.setDescription("Powder Blush");
-			cosmetic.setPrice(new BigDecimal(24.66));
-			cosmetics.put((long)i, cosmetic);
+
+			try (ResultSet rs = preparedStatement.executeQuery()) {
+				if (rs.next()) {
+					cosmetic.setId(rs.getInt("id"));
+					cosmetic.setName(rs.getString("name"));
+					cosmetic.setDescription(rs.getString("description"));
+					cosmetic.setBrand(rs.getString("brand"));
+					cosmetic.setPrice(rs.getBigDecimal("price"));
+
+					System.out.println(cosmetic.toString());
+				}
+				return cosmetic;
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
 		}
-		
-		Cosmetic target = cosmetics.get(id);
-		return target;
+		return null;
 	}
 
 	@Override
 	public Cosmetic saveCosmetic(Cosmetic cosmetic) {
 		System.out.println("jdbc save Cosmetic");
 		try (Connection connection = dataSource.getConnection()) {
-            String sql = "INSERT INTO cosmetic(id, name, description, brand, price) VALUES (?,?,?,?,?)";
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			String sql = "INSERT INTO Cosmetic(id, name, description, brand, price) VALUES (?,?,?,?,?)";
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
-            try {
+			try {
 				preparedStatement.setLong(1, cosmetic.getId());
 				preparedStatement.setString(2, cosmetic.getName());
-		        preparedStatement.setString(3, cosmetic.getDescription());
-		        preparedStatement.setString(4, cosmetic.getBrand());
-		        preparedStatement.setBigDecimal(5, cosmetic.getPrice());
-		        
-		        int rowsInserted = preparedStatement.executeUpdate();
-	            System.out.println(rowsInserted + " row(s) inserted.");
+				preparedStatement.setString(3, cosmetic.getDescription());
+				preparedStatement.setString(4, cosmetic.getBrand());
+				preparedStatement.setBigDecimal(5, cosmetic.getPrice());
+
+				int rowsInserted = preparedStatement.executeUpdate();
+				System.out.println(rowsInserted + " row(s) inserted.");
 			} catch (SQLException e) {
 				e.printStackTrace();
-			}      
+			}
 		} catch (SQLException e1) {
 			e1.printStackTrace();
 		}
